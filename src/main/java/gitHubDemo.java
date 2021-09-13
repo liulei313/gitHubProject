04069502f50c355a3ee3898b23181b397dfcefae
liulei168 <314282435@qq.com>
2021-09-13 15:23:21 +0800

    Initial commit

---
+++ src/main/java/gitHubDemo.java
@@ -0,0 +1,13 @@
+/**
+ * @Project: gitHubProject
+ * @Copyright: ©2021 版权所有 LL
+ * @Author: admin
+ * @Create: 2021-09-13 15:16
+ * @Desc：
+ **/
+
+public class gitHubDemo {
+    public static void main(String[] args){
+        System.out.println("hello world git!");
+    }
+}
